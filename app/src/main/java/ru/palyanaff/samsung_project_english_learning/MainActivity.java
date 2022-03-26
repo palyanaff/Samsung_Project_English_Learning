b10c60a34a22337e3cb8bccf6f0f2c0beef2e09f
palyanaff <91470147+palyanaff@users.noreply.github.com>
2022-03-26 23:16:58 +0300

    restyle main screen

--- app/src/main/java/ru/palyanaff/samsung_project_english_learning/MainActivity.java
+++ app/src/main/java/ru/palyanaff/samsung_project_english_learning/MainActivity.java
@@ -3,7 +3,10 @@ package ru.palyanaff.samsung_project_english_learning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        ImageButton button = findViewById(R.id.levels_button);
+        button.setOnClickListener(this::openLevel);
     }
+
+    public void openLevel(View view)
+    {
+        Toast.makeText(this, "Done", Toast.LENGTH_LONG).show();
+    }
+
 }
