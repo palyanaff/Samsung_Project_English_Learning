1cda664d8a3c73a1ee9aefb13a1fc542d413706b
palyanaff <91470147+palyanaff@users.noreply.github.com>
2022-04-03 00:19:54 +0300

    Add fragments and navigation bar

--- app/src/main/java/ru/palyanaff/samsung_project_english_learning/MainActivity.java
+++ app/src/main/java/ru/palyanaff/samsung_project_english_learning/MainActivity.java
@@ -1,27 +1,62 @@
 package ru.palyanaff.samsung_project_english_learning;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
+
+import ru.palyanaff.samsung_project_english_learning.databinding.ActivityMainBinding;
+import ru.palyanaff.samsung_project_english_learning.screens.DictionaryFragment;
+import ru.palyanaff.samsung_project_english_learning.screens.ExamsFragment;
+import ru.palyanaff.samsung_project_english_learning.screens.LevelsFragment;
+import ru.palyanaff.samsung_project_english_learning.screens.MenuFragment;
+import ru.palyanaff.samsung_project_english_learning.screens.RunnerFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new LevelsFragment());
 
-        ImageButton button = findViewById(R.id.levels_button);
-        button.setOnClickListener(this::openLevel);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.dictionary:
+                    replaceFragment(new DictionaryFragment());
+                    break;
+                case R.id.exams:
+                    replaceFragment(new ExamsFragment());
+                    break;
+                case R.id.level:
+                    replaceFragment(new LevelsFragment());
+                    break;
+                case R.id.menu:
+                    replaceFragment(new MenuFragment());
+                    break;
+                case R.id.runner:
+                    replaceFragment(new RunnerFragment());
+                    break;
+
+            }
+
+            return true;
+        });
+
     }
 
-    public void openLevel(View view)
-    {
-        Toast.makeText(this, "Done", Toast.LENGTH_LONG).show();
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 
 }
